30bb05b42621066a84b4a3b10e23308393663550
RanieryAV <58216614+RanieryAV@users.noreply.github.com>
2022-06-13 21:48:49 -0300

    Banco de Dados "equipe473532" criado
    - Por meio do MySQL Workbench, criei um arquivo .sql com os dois comandos que criam (CREATE DATABASE) e selecionam para uso (USE) o banco de dados do arquivo "equipe473532.sql".
    
    - O nome do banco de dados foi definido conforme o descritivo do Trabalho 02 enviado pelo professor Fernando.

---
+++ equipe473532.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE equipe473532; # Criar banco de dados com o nome solicitado pelo professor
+USE equipe473532; # Comando necessário para começar a criar tabelas no banco de dados
+
+# A partir daqui, começam as definições das tabelas pedidas no descritivo do trabalho
