a4f9d29a95a7c3912105ccc16bf2998400779a31
WesllenVS <wesllenvasconcelossousa@hotmail.com>
2022-06-20 20:23:48 -0300

    Criação das Entidades "Categoria", "Livros", "Autor" e o relacionamento "Escreve"
    Foi criado as entidades "Categoria" e informado a chave primária codCategoria, além de ser relacionada com a entidade livros;
    Criado a entidade "Livros" e informado a chave primária o "ISBN"e ela se relaciona com autor e com a categoria
    Por fim, criado a Entidade "Autor" que tem como chave primária o "E-mail" e se relaciona com "Livros

--- equipe473532.sql
+++ equipe473532.sql
@@ -18,15 +18,67 @@ CREATE SCHEMA IF NOT EXISTS `equipe473532` DEFAULT CHARACTER SET utf8mb4 COLLATE
 USE `equipe473532` ;
 
 -- -----------------------------------------------------
--- Table `equipe473532`.`categoria_livro`
+-- Table `equipe473532`.`Categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `equipe473532`.`categoria_livro` (
-  `codigo_da_categoria` INT NOT NULL,
-  `descricao` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`codigo_da_categoria`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `equipe473532`.`Categoria` (
+  `codCategoria` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`codCategoria`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `equipe473532`.`Livros`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `equipe473532`.`Livros` (
+  `ISBN` INT NOT NULL,
+  `titulo` VARCHAR(90) NOT NULL,
+  `anoLancamento` YEAR NOT NULL,
+  `editora` VARCHAR(50) NOT NULL,
+  `quantidadeCopias` BIGINT NOT NULL,
+  `Categoria_codCategoria` INT NOT NULL,
+  PRIMARY KEY (`ISBN`),
+  UNIQUE INDEX `ISBN_UNIQUE` (`ISBN` ASC),
+  INDEX `fk_Livros_Categoria1_idx` (`Categoria_codCategoria` ASC),
+  CONSTRAINT `fk_Livros_Categoria1`
+    FOREIGN KEY (`Categoria_codCategoria`)
+    REFERENCES `equipe473532`.`Categoria` (`codCategoria`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `equipe473532`.`Autores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `equipe473532`.`Autores` (
+  `email` VARCHAR(60) NOT NULL,
+  `nome` VARCHAR(60) NOT NULL,
+  `nacionalidade` VARCHAR(40) NOT NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `equipe473532`.`Escreve`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `equipe473532`.`Escreve` (
+  `Autor_email` VARCHAR(60) NOT NULL,
+  `Livros_ISBN` INT NOT NULL,
+  PRIMARY KEY (`Autor_email`, `Livros_ISBN`),
+  INDEX `fk_Autor_has_Livros_Livros1_idx` (`Livros_ISBN` ASC),
+  INDEX `fk_Autor_has_Livros_Autor_idx` (`Autor_email` ASC),
+  CONSTRAINT `fk_Autor_has_Livros_Autor`
+    FOREIGN KEY (`Autor_email`)
+    REFERENCES `equipe473532`.`Autores` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Autor_has_Livros_Livros1`
+    FOREIGN KEY (`Livros_ISBN`)
+    REFERENCES `equipe473532`.`Livros` (`ISBN`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -76,7 +128,6 @@ CREATE TABLE IF NOT EXISTS `equipe473532`.`alunos` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `equipe473532`.`telefones_alunos`
@@ -158,33 +209,35 @@ CREATE TABLE IF NOT EXISTS `equipe473532`.`telefones_funcionarios` (
 ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `equipe473532`.`autores`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `equipe473532`.`autores` (
-  `email` VARCHAR(45) NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `nacionalidade` VARCHAR(45) NULL,
-  PRIMARY KEY (`email`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `equipe473532`.`livros_tem_autores`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `equipe473532`.`livros_tem_autores` (
-  `ISBN` BIGINT(13) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  UNIQUE INDEX `ISBN_UNIQUE` (`ISBN` ASC) VISIBLE,
-  UNIQUE INDEX `email_autor_UNIQUE` (`email` ASC) VISIBLE,
-  CONSTRAINT `email`
-    FOREIGN KEY (`email`)
-    REFERENCES `equipe473532`.`autores` (`email`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+-- -----------------------------------------------------
+-- Povoamento das tabelas
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Povoando a tabela de categoria
+-- -----------------------------------------------------
+
+INSERT INTO categoria VALUES(1, 'Ação');
+INSERT INTO categoria VALUES(2, 'Ficção');
+INSERT INTO categoria VALUES(3, 'Romance');
+INSERT INTO categoria VALUES(4, 'Infantil');
+INSERT INTO categoria VALUES(5, 'Fantasia');
+INSERT INTO categoria VALUES(6, 'Auto-Ajuda');
+INSERT INTO categoria VALUES(7, 'Administração');
+INSERT INTO categoria VALUES(8, 'Terror');
+
+
+-- -----------------------------------------------------
+-- Povoando a tabela de Autores 
+-- -----------------------------------------------------
+
+INSERT INTO autor VALUES('anabeatrizbarbosa@hotmail.com', 'Ana Beatriz Barbosa Silva', 'Brasileira');
+INSERT INTO autor VALUES('rjpalacio@hotmail.com','R J Palacio', 'Americana');
+INSERT INTO autor VALUES('joaquimmanuelmacedo@hotmail.com','Joaquim Manuel de Macedo', 'Brasileiro');
+INSERT INTO autor VALUES('rhondabyrne@hotmail.com','Rhonda Byrne ', 'Australiana');
+INSERT INTO autor VALUES('suzannecollins@hotmail.com','Suzanne Collins', 'Americana');
